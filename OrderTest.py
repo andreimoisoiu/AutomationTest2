a4246a06d7a65217b56162607d9fec97aafd6821
Andrei <moisoiuaandrei@gmail.com>
2021-06-21 20:46:02 +0300

    open browser and navigate to url

--- OrderTest.py
+++ OrderTest.py
@@ -1,22 +1,5 @@
 from selenium import webdriver
 
 
-def test():
-    name = "Andrei"
-    print(name)
-
-test()
-
-def test2():
-    name = "LOl"
-    print(name)
-
-
-test2()
-
-def test3():
-    name = "LLLL"
-    print(name)
-
-
-test3()
+driver = webdriver.Firefox()
+driver.get("https://www.google.com")
